ff26f57b2137d3b5f6500b192f5974897b876d98
hereWasKitus <nekitdragon@gmail.com>
2022-11-20 21:07:48 +0200

    Sidebar ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
-import { useTasksStore } from './store';
-import './assets/styles/note.css';
-import TopBar from './assets/components/TopBar/TopBar';
-import SideBar from './assets/components/SideBar/SideBar';
-import Workspace from './assets/components/Workspace/Workspace';
-import { Note } from './note'
+import { useTasksStore } from "./store";
+import "./assets/styles/note.css";
+import TopBar from "./assets/components/TopBar/TopBar";
+import SideBar from "./assets/components/SideBar/SideBar";
+import Workspace from "./assets/components/Workspace/Workspace";
+import { Note } from "./note";
 
 function App() {
-  const tasks = useTasksStore((state) => state.tasks)
+  const tasks = useTasksStore((state) => state.tasks);
 
   return (
     <div className="note-app">
       <TopBar />
       <div className="note-container">
-        <SideBar notes={[new Note(1, 'Вынести мусор', 'не вышло')]} />
+        <SideBar />
         <Workspace />
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/assets/components/SideBar/SideBar.jsx
+++ src/assets/components/SideBar/SideBar.jsx
@@ -1,29 +1,32 @@
-function SideBar({notes=[]}) {
-  if(notes.length == 0){
+import { Note } from "../../../note.js";
+import { useState } from "react";
+import style from "./SideBar.module.css";
+
+function SideBar() {
+  const [notes, setNotes] = useState([
+    new Note(1, "Вынести мусор", "не вышло"),
+  ]);
+
+  if (notes.length == 0) {
     return (
-      <ul className="note-sidebar">
-          <li className="note active">
-            <h2>Заметку забыл создать ГОНДОН!</h2>
-          </li>
+      <ul className={style.noteList}>
+        <li className="note active">
+          <h2>Заметку забыл создать ГОНДОН!</h2>
+        </li>
       </ul>
-    )
+    );
   }
 
-  
-    return (
-      <ul className="note-sidebar">
-        
-        {notes.map(item => (
-          <li key={item.id} className="note active">
-            <h2>{item.name}</h2>
-            <p>{item.dateCreated.toLocaleDateString("en-US")}</p>
-          </li>
-          ))}
-      </ul>
-    )
-  }
+  return (
+    <ul className={style.noteList}>
+      {notes.map((item) => (
+        <li key={item.id} className={style.note}>
+          <h2>{item.name}</h2>
+          <p>{item.dateCreated.toLocaleDateString("en-US")}</p>
+        </li>
+      ))}
+    </ul>
+  );
+}
 
-
-
-
-export default SideBar
+export default SideBar;
